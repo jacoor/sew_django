148c41979a04da6c13482bd710ddb63eb5329aa7
Jacek <jacek@ivolution.pl>
2013-12-10 23:23:32 +0100

    initial, still

--- sass.py
+++ sass.py
@@ -1,5 +1,5 @@
 from subprocess import call
 import os
 cwd = os.getcwd()
-command = "sass --scss --debug-info --compass --watch %s/styleguide/static/scss/:%s/styleguide/static/.sass-cache/livereload/" % (cwd,cwd)
+command = "sass --scss --debug-info --compass --watch %s/sew_django/static/scss/:%s/sew_django/static/.sass-cache/livereload/" % (cwd,cwd)
 os.system(command)

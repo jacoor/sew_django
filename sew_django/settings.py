cf2e87af7fd5d96f681f772defe078aaa0a7cf8c
Jacek <jacek@ivolution.pl>
2013-12-14 23:59:31 +0100

    conf

--- sew_django/settings.py
+++ sew_django/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'south',
 #    'ydcommon',
+    'tinymce',
     'compressor',
     'raven.contrib.django.raven_compat',
 )
--- sew_django/urls.py
+++ sew_django/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -7,12 +8,14 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'sew_django.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r"^admin/", include(admin.site.urls)),
     (r'^%s(?P<path>.*)$' % settings.STATIC_URL[1:],  # cut away leading slash
          'django.views.static.serve', {'document_root': settings.STATIC_ROOT,
                                        'show_indexes': True}),
-        (r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:],  # cut away leading slash
+    (r'^%s(?P<path>.*)$' % settings.MEDIA_URL[1:],  # cut away leading slash
          'django.views.static.serve', {'document_root': settings.MEDIA_ROOT,
                                        'show_indexes': True}),
+
+    #(r'^grappelli/', include('grappelli.urls')),
+    (r'^tinymce/', include('tinymce.urls')),
 )

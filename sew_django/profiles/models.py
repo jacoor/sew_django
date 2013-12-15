d5cd6b7435c62a49ba2907cf6d89cc8b50c53709
Jacek <jacek@ivolution.pl>
2013-12-15 14:57:15 +0100

    basic authentication with email

--- sew_django/profiles/models.py
+++ sew_django/profiles/models.py
@@ -1,7 +1,28 @@
 from django.db import models
 from django.conf import settings
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 # Create your models here.
+
+class ProfileManager(BaseUserManager):
+    def create_user(self, email=None, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The given email must be set')
+        email = ProfileManager.normalize_email(email)
+        user = self.model(email=email,
+                          is_staff=False, is_active=True, is_superuser=False,
+                          **extra_fields)
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, **extra_fields):
+        u = self.create_user(email, password, **extra_fields)
+        u.is_staff = True
+        u.is_active = True
+        u.is_superuser = True
+        u.save(using=self._db)
+        return u
 
 class Profile(AbstractBaseUser):
     email = models.EmailField('adres email', max_length=110, unique=True)
@@ -12,3 +33,15 @@ class Profile(AbstractBaseUser):
     token = models.CharField(max_length=40, blank=True)
 
     USERNAME_FIELD = 'email'
+
+    objects = ProfileManager()
+    
+    is_staff = models.BooleanField('staff status', default=False,
+        help_text='Designates whether the user can log into this admin '
+                    'site.')
+    is_active = models.BooleanField('active', default=True,
+        help_text='Designates whether this user should be treated as '
+                    'active. Unselect this instead of deleting accounts.')
+
+    def get_by_natural_key(self, username):
+        return self.get(**{self.model.USERNAME_FIELD: username})

76cc72db1e36a37beef328152d5ab67b12447d56
Jacek <jacek@ivolution.pl>
2013-12-15 15:44:15 +0100

    tests

--- sew_django/profiles/tests.py
+++ sew_django/profiles/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+# -*- coding: utf-8 -*-
+from django.test import TestCase, RequestFactory, SimpleTestCase
+from django.core.urlresolvers import reverse
+from django.test.client import Client
 
-# Create your tests here.
+from sew_django.profiles.models import Profile
+from sew_django.profiles.views import Index
+
+
+
+def setup_view(view, request, *args, **kwargs):
+    """*args and **kwargs you could pass to ``reverse()``."""
+    view.request = request
+    view.args = args
+    view.kwargs = kwargs
+    return view
+
+
+class ProfileTests(TestCase):
+
+    def setUp(self):
+        self.user = Profile.objects.create(names='Joe',
+                                            email='joe@doe.com',
+                                            is_active=True,
+                                            phone='111111')
+        self.user.set_password('dump-password')
+        self.user.save()
+
+    def test_index(self):
+        #request = RequestFactory().get(reverse('index'))
+        c = Client()
+        responce = c.get('/')
+        #response.status_code
+        self.assertEqual(response.status_code, 200)
+

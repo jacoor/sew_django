b6532af2bb0284d69274f2f4e417ed4fc8a1ff27
Jacek <jacek@ivolution.pl>
2013-12-15 03:54:56 +0100

    after tests grunt showed being unusable for us.

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function(grunt) {
                     debugInfo : true
                 },
                 files: {
-                    '.sass-cache/livereload/main.css': 'sew_django/static/scss/main.scss'
+                    'sew_django/static/.sass-cache/livereload/main.css': 'sew_django/static/scss/main.scss'
                 }
             } 
         },
@@ -28,6 +28,6 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.registerTask('default', [ 'sass']);
+    grunt.registerTask('default', [ 'sass', 'watch']);
 };
 

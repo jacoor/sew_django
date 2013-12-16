6d9bdb2729ddec99e6adba4cb4eba7e472e9ce36
Jacek <jacek@ivolution.pl>
2013-12-17 00:15:20 +0100

    better errors

--- sass.py
+++ sass.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python
 from subprocess import call
 import os
 cwd = os.getcwd()
-command = "sass --scss --debug-info --compass --watch %s/sew_django/static/scss/:%s/sew_django/static/.sass-cache/livereload/" % (cwd,cwd)
+command = "sass --scss --debug-info --compass --watch %s/sew_django/static/scss/:%s/sew_django/static/.sass-cache/livereload" % (cwd,cwd)
 os.system(command)

6b19db4b20b924b643e0e78d18893d6e0aec62b9
Jacek <jacek@ivolution.pl>
2013-12-18 13:05:21 +0100

    custom admin login form

--- sew_django/urls.py
+++ sew_django/urls.py
@@ -3,11 +3,18 @@ from django.conf import settings
 from django.views.generic import TemplateView
 
 from django.contrib import admin
+
+from sew_django.profiles.forms import AuthenticationForm
 admin.autodiscover()
+admin.site.login_form = AuthenticationForm
+admin.site.site_title = "System Ewidencji Wolontariuszy"
+admin.site.site_header = admin.site.site_title
+admin.site.index_title = admin.site.site_title
 
 urlpatterns = patterns('',
     url(r'', include('sew_django.profiles.urls')),
     url(r"^admin/", include(admin.site.urls)),
+
     (r'^%s(?P<path>.*)$' % settings.STATIC_URL[1:],  # cut away leading slash
          'django.views.static.serve', {'document_root': settings.STATIC_ROOT,
                                        'show_indexes': True}),

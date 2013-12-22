0d121d63aee99542453b407f64df118c68471174
Jacek <jacek@ivolution.pl>
2013-12-22 20:14:02 +0100

    added pesel validator

---
+++ sew_django/profiles/backends/PeselAuthBackend.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.contrib.auth.backends import ModelBackend
+from django.contrib.auth import get_user_model
+
+
+class EmailAuthBackend(ModelBackend):
+    def authenticate(self, username=None, password=None, **kwargs):
+        UserModel = get_user_model()
+        if username is None:
+            username = kwargs.get('pesel')
+        try:
+            user = UserModel._default_manager.get_by_pesell(username)
+            if user.check_password(password):
+                return user
+        except UserModel.DoesNotExist:
+            UserModel().set_password(password)
